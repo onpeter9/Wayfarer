b38e64242b86219b70a53d4fb1ad68cce7cd2d75
Peter Ebuzome <onpeter9@gmail.com>
2019-07-15 20:27:54 +0100

    feat(endpoint): create the get all trips endpoint
     - setup mocha tests for the get all trips endpoint
     - created the get all trips endpoint
     - tested the get all trips endpoint with postman
    
    [Finishes #167300105]

--- app/test/user.test.js
+++ app/test/user.test.js
@@ -143,57 +143,6 @@ describe('user signup error invalid email', () => {
   });
 });
 
-describe('user signin no error', () => {
-  it('should successfully signin a user with a registered email and valid password', (done) => {
-    request(app)
-      .post('/api/v1/auth/signin')
-      .send({ email: config.user, password: config.userPassword })
-      .end((err, res) => {
-        expect(res).to.be.an('object');
-        res.should.haveOwnProperty('status');
-        res.status.should.equal(200);
-        const { body } = res;
-        body.should.haveOwnProperty('status');
-        body.status.should.be.a('string');
-        body.status.should.equal('success');
-        body.should.haveOwnProperty('data');
-        const { data } = body;
-        data.should.be.an('object');
-        data.should.haveOwnProperty('token');
-        data.token.should.be.a('string');
-        data.should.haveOwnProperty('id');
-        data.id.should.be.a('string');
-        data.should.haveOwnProperty('email');
-        data.email.should.be.a('string');
-        data.email.should.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
-        data.should.haveOwnProperty('is_admin');
-        data.is_admin.should.be.a('boolean');
-        done();
-      });
-  });
-});
-
-
-describe('user signin error unregistered email', () => {
-  it('should unsuccessfully signin a user with unregistered email', (done) => {
-    request(app)
-      .post('/api/v1/auth/signin')
-      .send({ email: 'sparkn@aol.com', password: 'Insane2030' })
-      .end((err, res) => {
-        expect(res).to.haveOwnProperty('status');
-        expect(res.status).to.be.a('number');
-        expect(res.status).to.be.equal(401);
-        const { body } = res;
-        expect(body).to.be.an('object');
-        expect(body).to.haveOwnProperty('status');
-        expect(body.status).to.be.a('string');
-        expect(body.status).to.be.equal('error');
-        expect(body).to.haveOwnProperty('error');
-        expect(body.error).to.be.equal('Unauthorized access!');
-        done();
-      });
-  });
-});
 
 describe('user signin error wrong password', () => {
   it('should unsuccessfully signin a user with a registered email but a wrong password', (done) => {
